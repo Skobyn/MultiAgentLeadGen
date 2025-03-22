ecd7e5d547da90d281d2947fca486c71903579ab
Skobyn <129495610+Skobyn@users.noreply.github.com>
2025-03-22 15:23:44 -0500

    Fix ESLint errors in Settings.tsx

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -427,12 +427,12 @@ const Settings: React.FC = () => {
                                 </span>
                               </td>
                               <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a
-                                  href="#"
+                                <button
+                                  type="button"
                                   className="text-primary-600 hover:text-primary-900"
                                 >
                                   Edit
-                                </a>
+                                </button>
                               </td>
                             </tr>
                           ))}
